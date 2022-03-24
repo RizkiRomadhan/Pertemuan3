1a123a63d0e984a48ebb3d60472fa13b5654503a
ACER <rizkiromadhanbks298@gmail.com>
2022-03-24 11:04:30 +0700

    Ini adalah sebuah kode program yang digunakan untuk menjalankan Greeter

---
+++ src/main/kotlin/id/ac/polbeng/rizkiromadhan_test/kelas/Greeter.kt
@@ -0,0 +1,12 @@
+package id.ac.polbeng.rizkiromadhan_test.kelas
+
+class Greeter {
+    fun greet() {
+        println("Hello object world!")
+    }
+}
+
+fun main() {
+    val greeter = Greeter()
+    greeter.greet()
+}
