de9f5705aad6e61534dc73297e7ec87bb5e57b4a
ACER <rizkiromadhanbks298@gmail.com>
2022-03-24 11:53:32 +0700

    Ini adalah kode program untuk menjalankan greeter test

--- src/main/kotlin/id/ac/polbeng/rizkiromadhan_test/kelas/GreeterTest.kt
+++ src/main/kotlin/id/ac/polbeng/rizkiromadhan_test/kelas/GreeterTest.kt
@@ -1,4 +1,11 @@
 package id.ac.polbeng.rizkiromadhan_test.kelas
 
-class GreeterTest {
+fun main() {
+    val greeter = Greeter()
+    greeter.greet()
+    greeter.text = "Hi"
+    greeter.greet("Rizki")
+    greeter.greet("Romadhan")
+    greeter.text = "Hello programmer"
+    println(greeter.with_ret_val("Kekek"))
 }
